24c30548b290bb3277f12d2bac28a03c260a3afd
micheleko23 <ko.micheletw@gmail.com>
2020-07-14 22:19:03 +0800

    2.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 
 
 console.log ("Hi there, welcome!")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+  
+}
